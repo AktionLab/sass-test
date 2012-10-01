2982ac67284fa9d7f174e24b9362ccbe90f33aed
Daniel <daniel.zukowski@gmail.com>
2012-09-30 23:57:05 -0600

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SassTest::Application.config.secret_token = '653383712e6f523307a29d3fb753d7d6498e50212e207549ecd39939c44e2613c622c0e7589b86f28eff6329a2f20ba15a9efbd625646e9bcefa900a8e3e6198'
